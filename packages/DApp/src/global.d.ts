256b9a2215fce231ffc483e408fb34e7eac27129
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2023-05-02 16:05:56 +0200

    update types

--- packages/DApp/src/global.d.ts
+++
@@ -1,5 +0,0 @@
-declare namespace NodeJS {
-  export interface ProcessEnv {
-    ENV: 'localhost' | 'development' | 'production'
-  }
-}
--- packages/DApp/src/vite-env.d.ts
+++ packages/DApp/src/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    ENV: 'localhost' | 'development' | 'production'
+  }
+}
